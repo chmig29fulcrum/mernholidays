be6f264c35c37761c465625cbbed7ac11dc81607
CH <chmig29fulcrum@gmail.com>
2024-01-27 22:06:48 +0400

    added auth & auth tests & initial layout

---
+++ backend/src/index.ts
@@ -0,0 +1,37 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import path from "path";
+
+import userRoutes from "./routes/users";
+import authRoutes from "./routes/auth";
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
+//.then(() =>console.log("connected to database: ",process.env.MONGODB_CONNECTION_STRING));
+
+const app = express();
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+
+//app.get("/api/test", async (req: Request, res:Response)=>{
+//    res.json({message: "hello from express endpoint!"});
+//});
+
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
+app.listen(7000, () => {
+  console.log("server running on localhost:7000");
+  console.log(process.env.DOTENV_CONFIG_PATH);
+  //  console.log(process.env.NODE_ENV);
+});
